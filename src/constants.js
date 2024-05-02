841532e556ee24f241d72d718a83d238fffab818
Vulnpire <89225495+Vulnpire@users.noreply.github.com>
2024-05-02 15:42:42 +0200

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,29 @@
+// Constants
+export const scaleFactor = 4;
+
+// Dialogue Data
+export const dialogueData = {
+  pc: `This is my PC. I mainly work with Python and Bash these days, having completed several projects in Python. I also have an interest in JavaScript and Ruby. Regardless of the language, I enjoy programming and cybersecurity. I actively participate in CTFs on platforms like HackTheBox and TryHackMe to stay updated on recent threats. Check out my <a href="https://github.com/Vulnpire" target="_blank">Github</a> for more!`,
+
+  certs: `This is my eJPT certification. It was too easy for me to pass; I plan to pursue the OSCP soon.`,
+
+  "sofa-table": `That's my sofa. I relax here and play games on PlayStation.`,
+
+  tv: `This is my TV. Recently, I've been watching tech YouTubers like:
+    <a href="https://www.youtube.com/@_JohnHammond" target="_blank">John Hammond</a>,
+    <a href="https://www.youtube.com/@NahamSec" target="_blank">Nahamsec</a>,
+    <a href="https://www.youtube.com/@TCMSecurityAcademy" target="_blank">TCM</a>, and
+    <a href="https://www.youtube.com/@freecodecamp" target="_blank">Freecodecamp</a>!`,
+
+  bed: `This is where I sleep. I often get great ideas while lying here. When inspiration strikes, I jot it down; otherwise, it consumes my mental energy and keeps me awake.`,
+
+resume: `Welcome to my desk! Here's where you'll find my resume. 
+  If you have any exciting job opportunities, feel free to reach out to me 
+  via <a href="mailto://ones.and.zeroes.1@proton.me" target="_blank">email</a>.`,
+
+  projects: `About this portfolio: It's built with Kaboom.js, a library for JavaScript game development. Text is rendered using HTML/CSS outside of the canvas.`,
+
+  library: `I have many programming and hacking books on my shelves. I may have only read one of them. Who else buys technical books compulsively without finishing them?`,
+
+  exit: `To exit my portfolio, simply close the tab.`,
+};
